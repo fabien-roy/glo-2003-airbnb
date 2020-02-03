aa8f6272563e85b742bb36dc0cd1fb9bbd94b53b
Fabien Roy <fabienroy28@gmail.com>
2020-02-03 13:47:50 -0500

    Setup basic Spark Hello World

---
+++ src/main/java/ca/ulaval/glo2003/Main.java
@@ -0,0 +1,9 @@
+package ca.ulaval.glo2003;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
