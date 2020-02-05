cf7a4e2b9a57cf425a2df295aceef239a5f41f72
Fabien Roy <fabienroy28@gmail.com>
2020-02-04 20:36:38 -0500

    Fix test

--- src/test/java/ca/ulaval/glo2003/MainTest.java
+++ src/test/java/ca/ulaval/glo2003/MainTest.java
@@ -1,6 +1,5 @@
 package ca.ulaval.glo2003;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -10,12 +9,5 @@ public class MainTest {
   @Test
   public void given_when_then() {
     assertTrue(true); // TODO : Remove dummy test
-  }
-
-  @Test
-  public void aTest() {
-    Main main = new Main();
-
-    assertEquals(3, main.test("hello"));
   }
 }
