46595cd1b999bbb946698d3fcf8543d416f7cc9b
Fabien Roy <fabienroy28@gmail.com>
2020-02-16 16:16:01 -0500

    [AUL] Add BedResource.add(...) tests

--- src/test/java/ca/ulaval/glo2003/beds/rest/BedResourceTest.java
+++ src/test/java/ca/ulaval/glo2003/beds/rest/BedResourceTest.java
@@ -1,10 +1,16 @@
 package ca.ulaval.glo2003.beds.rest;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 import ca.ulaval.glo2003.beds.services.BedService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.eclipse.jetty.http.HttpHeader;
+import org.eclipse.jetty.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import spark.Request;
+import spark.Response;
 
 public class BedResourceTest {
 
@@ -18,13 +24,32 @@ public class BedResourceTest {
   }
 
   @Test
-  public void add_shouldBedNumberAsHeaderLocation() {
-    // TODO
+  public void add_shouldBedNumberAsHeaderLocation() throws JsonProcessingException {
+    String expectedBedNumber = "expectedBedNumber";
+    String expectedHeaderLocation = "/beds/" + expectedBedNumber;
+    Request request = mock(Request.class);
+    BedRequest bedRequest = mock(BedRequest.class);
+    String requestBody = new ObjectMapper().writeValueAsString(bedRequest);
+    when(request.body()).thenReturn(requestBody);
+    Response response = mock(Response.class);
+    when(bedService.add(any())).thenReturn(expectedBedNumber);
+
+    bedResource.add(request, response);
+
+    verify(response).header(HttpHeader.LOCATION.asString(), expectedHeaderLocation);
   }
 
   @Test
-  public void add_shouldSetCreatedAsHttpStatus() {
-    // TODO
+  public void add_shouldSetCreatedAsHttpStatus() throws JsonProcessingException {
+    Request request = mock(Request.class);
+    BedRequest bedRequest = mock(BedRequest.class);
+    String requestBody = new ObjectMapper().writeValueAsString(bedRequest);
+    when(request.body()).thenReturn(requestBody);
+    Response response = mock(Response.class);
+
+    bedResource.add(request, response);
+
+    verify(response).status(HttpStatus.CREATED_201);
   }
 
   @Test
