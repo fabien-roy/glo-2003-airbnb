f22720e9042f1ec01f387e16bae1a9ce42f09491
vince2848 <vince2848@msn.com>
2020-02-23 17:57:36 -0500

    [AUL] Add request with bed type

--- src/main/java/ca/ulaval/glo2003/beds/rest/mappers/BedMapper.java
+++ src/main/java/ca/ulaval/glo2003/beds/rest/mappers/BedMapper.java
@@ -1,7 +1,7 @@
 package ca.ulaval.glo2003.beds.rest.mappers;
 
+import ca.ulaval.glo2003.beds.domain.*;
 import ca.ulaval.glo2003.beds.domain.Bed;
-import ca.ulaval.glo2003.beds.domain.BedTypes;
 import ca.ulaval.glo2003.beds.rest.BedRequest;
 import ca.ulaval.glo2003.beds.rest.BedResponse;
 import java.util.ArrayList;
@@ -11,7 +11,6 @@ public class BedMapper {
   public Bed fromRequest(BedRequest request) {
     BedTypes bedType = BedTypes.get(request.getBedType());
 
-    // TODO : Add other values in BedMapper.fromRequest
     return new Bed(bedType, null, new ArrayList<>(), 0, new ArrayList<>());
   }
 
