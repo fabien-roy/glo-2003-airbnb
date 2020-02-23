59f507239f726da09fe6cf5b67ea93443bbd5d1e
vince2848 <vince2848@msn.com>
2020-02-23 17:57:06 -0500

    [AUL] Add invalid format exception

--- src/main/java/ca/ulaval/glo2003/interfaces/rest/ErrorResource.java
+++ src/main/java/ca/ulaval/glo2003/interfaces/rest/ErrorResource.java
@@ -4,7 +4,9 @@ import static spark.Spark.exception;
 
 import ca.ulaval.glo2003.beds.rest.exceptions.*;
 import ca.ulaval.glo2003.beds.rest.handlers.*;
+import ca.ulaval.glo2003.interfaces.rest.exceptions.InvalidFormatException;
 import ca.ulaval.glo2003.interfaces.rest.handlers.CatchallExceptionHandler;
+import ca.ulaval.glo2003.interfaces.rest.handlers.InvalidFormatExceptionHandler;
 import ca.ulaval.glo2003.interfaces.rest.handlers.JsonProcessingExceptionHandler;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import spark.RouteGroup;
@@ -17,6 +19,7 @@ public class ErrorResource implements RouteGroup {
   public void addRoutes() {
     exception(Exception.class, new CatchallExceptionHandler());
     exception(JsonProcessingException.class, new JsonProcessingExceptionHandler());
+    exception(InvalidFormatException.class, new InvalidFormatExceptionHandler());
     exception(InvalidBedTypeException.class, new InvalidBedTypeExceptionHandler());
     exception(BedNotFoundException.class, new BedNotFoundExceptionHandler());
     exception(
