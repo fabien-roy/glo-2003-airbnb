399623f8864752bc65476944c142ddf5ded27f35
Fabien Roy <fabienroy28@gmail.com>
2020-02-23 02:04:05 -0500

    [AUL] Add a tests for BedStarsCalculator

--- src/test/java/ca/ulaval/glo2003/beds/domain/BedStarsCalculatorTest.java
+++ src/test/java/ca/ulaval/glo2003/beds/domain/BedStarsCalculatorTest.java
@@ -1,5 +1,64 @@
 package ca.ulaval.glo2003.beds.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static ca.ulaval.glo2003.beds.helpers.BedBuilder.aBed;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class BedStarsCalculatorTest {}
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class BedStarsCalculatorTest {
+
+  @ParameterizedTest
+  @MethodSource("provideConditionsForCalculateStars")
+  public void calculateStars_shouldReturnCorrectNumberOfStars(
+      BedTypes bedType,
+      CleaningFrequencies cleaningFrequency,
+      List<BloodTypes> bloodTypes,
+      int expectedStars) {
+    Bed bed =
+        aBed()
+            .withBedType(bedType)
+            .withCleaningFrequency(cleaningFrequency)
+            .withBloodTypes(bloodTypes)
+            .build();
+    BedStarsCalculator bedStarsCalculator = new BedStarsCalculator();
+
+    int stars = bedStarsCalculator.calculateStars(bed);
+
+    assertEquals(expectedStars, stars);
+  }
+
+  private static Stream<Arguments> provideConditionsForCalculateStars() {
+    return Stream.of(
+        Arguments.of(
+            BedTypes.LATEX,
+            CleaningFrequencies.WEEKLY,
+            Collections.singletonList(BloodTypes.O_MINUS),
+            3),
+        Arguments.of(
+            BedTypes.LATEX,
+            CleaningFrequencies.WEEKLY,
+            Collections.singletonList(BloodTypes.B_PLUS),
+            1),
+        Arguments.of(
+            BedTypes.MEMORY_FOAM,
+            CleaningFrequencies.ANNUAL,
+            Collections.singletonList(BloodTypes.AB_MINUS),
+            2),
+        Arguments.of(
+            BedTypes.SPRINGS,
+            CleaningFrequencies.NEVER,
+            Arrays.asList(BloodTypes.O_MINUS, BloodTypes.O_PLUS, BloodTypes.A_PLUS),
+            5),
+        Arguments.of(
+            BedTypes.LATEX,
+            CleaningFrequencies.ANNUAL,
+            Arrays.asList(BloodTypes.O_MINUS, BloodTypes.B_MINUS),
+            4));
+  }
+}
