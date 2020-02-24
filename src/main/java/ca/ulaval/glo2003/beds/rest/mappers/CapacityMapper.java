0467d9673653adb95157244c2a6aa896f5da2c3f
vince2848 <vince2848@msn.com>
2020-02-24 09:23:43 -0500

    Refactor CapacityMapper

--- src/main/java/ca/ulaval/glo2003/beds/rest/mappers/CapacityMapper.java
+++ src/main/java/ca/ulaval/glo2003/beds/rest/mappers/CapacityMapper.java
@@ -2,19 +2,33 @@ package ca.ulaval.glo2003.beds.rest.mappers;
 
 import ca.ulaval.glo2003.beds.domain.BedTypes;
 import ca.ulaval.glo2003.beds.rest.BedRequest;
+import ca.ulaval.glo2003.beds.rest.exceptions.InvalidBedTypeException;
 import ca.ulaval.glo2003.beds.rest.exceptions.InvalidMaximalCapacityException;
 
 public class CapacityMapper {
 
-  static void validateCapacity(BedRequest bedRequest) {
-    if (bedRequest.getBedType().equals(BedTypes.LATEX.toString())) {
-      if (bedRequest.getCapacity() > 400) throw new InvalidMaximalCapacityException();
+  public static void validateCapacity(BedRequest bedRequest) {
+    BedTypes bedType = BedTypes.get(bedRequest.getBedType());
+    if (bedRequest.getCapacity() > getMaxCapacity(bedType)) {
+      throw new InvalidMaximalCapacityException();
     }
-    if (bedRequest.getBedType().equals(BedTypes.MEMORY_FOAM.toString())) {
-      if (bedRequest.getCapacity() > 700) throw new InvalidMaximalCapacityException();
+  }
+
+  public static int getMaxCapacity(BedTypes bedType) {
+    int maxCapacity;
+    switch (bedType) {
+      case LATEX:
+        maxCapacity = 400;
+        break;
+      case MEMORY_FOAM:
+        maxCapacity = 700;
+        break;
+      case SPRINGS:
+        maxCapacity = 1000;
+        break;
+      default:
+        throw new InvalidBedTypeException();
     }
-    if (bedRequest.getBedType().equals(BedTypes.SPRINGS.toString())) {
-      if (bedRequest.getCapacity() > 1000) throw new InvalidMaximalCapacityException();
-    }
+    return maxCapacity;
   }
 }
