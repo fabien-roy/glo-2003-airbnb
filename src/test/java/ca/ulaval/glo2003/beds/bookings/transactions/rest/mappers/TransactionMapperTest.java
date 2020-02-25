315258f5edf19a31c57108ea91122290b270faa0
Sandrine Diffo <sandrinediffo@Sandrines-MacBook-Pro.local>
2020-02-24 21:49:46 -0500

    add test for transactionMapper

--- src/test/java/ca/ulaval/glo2003/beds/bookings/transactions/rest/mappers/TransactionMapperTest.java
+++ src/test/java/ca/ulaval/glo2003/beds/bookings/transactions/rest/mappers/TransactionMapperTest.java
@@ -1,6 +1,13 @@
 package ca.ulaval.glo2003.beds.bookings.transactions.rest.mappers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import ca.ulaval.glo2003.beds.bookings.transactions.domain.Transaction;
+import ca.ulaval.glo2003.beds.bookings.transactions.domain.TransactionReasons;
+import ca.ulaval.glo2003.beds.bookings.transactions.rest.TransactionResponse;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TransactionMapperTest {
 
@@ -9,5 +16,25 @@ public class TransactionMapperTest {
   @BeforeEach
   public void setUpMapper() {
     transactionMapper = new TransactionMapper();
+  }
+
+  @Test
+  public void getTransaction_shouldReturnTransactionResponseWithSameParameters() {
+
+    int expectedTotal = 10;
+    String expectedTo = "transactionrecipent";
+    String expectedFrom = "transactionsender";
+    LocalDateTime expectedTimestamp = LocalDateTime.now();
+    TransactionReasons expectedReason = TransactionReasons.STAYBOOKED;
+
+    Transaction transactionToMap =
+        new Transaction(expectedReason, expectedTo, expectedFrom, expectedTotal, expectedTimestamp);
+    TransactionResponse response = transactionMapper.toResponse(transactionToMap);
+
+    assertEquals(response.getTimestamp(), expectedTimestamp);
+    assertEquals(response.getTransactionReasons(), expectedReason);
+    assertEquals(response.getFrom(), expectedFrom);
+    assertEquals(response.getTo(), expectedTo);
+    assertEquals(response.getTotal(), expectedTotal);
   }
 }
