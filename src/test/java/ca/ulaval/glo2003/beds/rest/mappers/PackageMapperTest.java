b0f3f293979b3b6333770cc9c52732e097bc6970
Fabien Roy <fabienroy28@gmail.com>
2020-02-25 10:01:59 -0500

    [AUL] Change tests for PackageMapper

--- src/test/java/ca/ulaval/glo2003/beds/rest/mappers/PackageMapperTest.java
+++ src/test/java/ca/ulaval/glo2003/beds/rest/mappers/PackageMapperTest.java
@@ -11,7 +11,11 @@ import ca.ulaval.glo2003.beds.domain.helpers.PackageObjectMother;
 import ca.ulaval.glo2003.beds.rest.PackageRequest;
 import ca.ulaval.glo2003.beds.rest.PackageResponse;
 import ca.ulaval.glo2003.beds.rest.exceptions.InvalidPackageException;
-import ca.ulaval.glo2003.beds.rest.helpers.PackageRequestObjectMother;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -25,32 +29,101 @@ class PackageMapperTest {
   }
 
   @Test
-  public void fromRequest_shouldMapName() {
-    String expectedName = PackageRequestObjectMother.createPackageName();
-    PackageRequest packageRequest = aPackageRequest().withName(expectedName).build();
+  public void fromRequests_withSingleRequest_shouldMapASinglePricePerNight() {
+    PackageRequest request = aPackageRequest().build();
+    List<PackageRequest> requests = Collections.singletonList(request);
 
-    Package testPackage = packageMapper.fromRequest(packageRequest);
+    Map<PackageNames, Price> pricesPerNight = packageMapper.fromRequests(requests);
 
-    assertEquals(expectedName, testPackage.getName().toString());
+    assertEquals(1, pricesPerNight.size());
   }
 
   @Test
-  public void fromRequest_withInvalidPackageName_shouldThrowInvalidPackageException() {
-    String invalidName = "invalidName";
-    PackageRequest packageRequest = aPackageRequest().withName(invalidName).build();
+  public void fromRequests_withMultipleRequests_shouldMapMultiplePricesPerNight() {
+    PackageRequest request = aPackageRequest().build();
+    PackageRequest otherRequest = aPackageRequest().build();
+    List<PackageRequest> requests = Arrays.asList(request, otherRequest);
 
-    assertThrows(InvalidPackageException.class, () -> packageMapper.fromRequest(packageRequest));
+    Map<PackageNames, Price> pricesPerNight = packageMapper.fromRequests(requests);
+
+    assertEquals(2, pricesPerNight.size());
   }
 
   @Test
-  public void fromRequest_shouldMapPricePerNight() {
-    double expectedPrice = PackageRequestObjectMother.createPricePerNight();
-    PackageRequest packageRequest = aPackageRequest().withPricePerNight(expectedPrice).build();
+  public void fromRequests_withSingleRequest_shouldMapName() {
+    PackageNames expectedPackage = PackageNames.BLOODTHIRSTY;
+    String packageName = expectedPackage.toString();
+    PackageRequest request = aPackageRequest().withName(packageName).build();
+    List<PackageRequest> requests = Collections.singletonList(request);
 
-    Package testPackage = packageMapper.fromRequest(packageRequest);
+    Map<PackageNames, Price> pricesPerNight = packageMapper.fromRequests(requests);
 
-    assertEquals(expectedPrice, testPackage.getPricePerNight().getValue().doubleValue());
+    assertTrue(pricesPerNight.containsKey(expectedPackage));
   }
+
+  @Test
+  public void fromRequests_withMultipleRequests_shouldMapNames() {
+    PackageNames expectedPackage = PackageNames.BLOODTHIRSTY;
+    PackageNames otherExpectedPackage = PackageNames.SWEET_TOOTH;
+    String packageName = expectedPackage.toString();
+    String otherPackageName = otherExpectedPackage.toString();
+    PackageRequest request = aPackageRequest().withName(packageName).build();
+    PackageRequest otherRequest = aPackageRequest().withName(otherPackageName).build();
+    List<PackageRequest> requests = Arrays.asList(request, otherRequest);
+
+    Map<PackageNames, Price> pricesPerNight = packageMapper.fromRequests(requests);
+
+    assertTrue(pricesPerNight.containsKey(expectedPackage));
+    assertTrue(pricesPerNight.containsKey(otherExpectedPackage));
+  }
+
+  @Test
+  public void fromRequests_withInvalidPackageName_shouldThrowInvalidPackageException() {
+    String invalidPackageName = "invalidPackageName";
+    PackageRequest request = aPackageRequest().withName(invalidPackageName).build();
+    List<PackageRequest> requests = Collections.singletonList(request);
+
+    assertThrows(InvalidPackageException.class, () -> packageMapper.fromRequests(requests));
+  }
+
+  @Test
+  public void fromRequests_withSingleRequest_shouldMapPricePerNight() {
+    double priceValue = 100.0;
+    Price expectedPrice = new Price(BigDecimal.valueOf(priceValue));
+    PackageNames packageName = PackageNames.BLOODTHIRSTY;
+    PackageRequest request =
+        aPackageRequest().withName(packageName.toString()).withPricePerNight(priceValue).build();
+    List<PackageRequest> requests = Collections.singletonList(request);
+
+    Map<PackageNames, Price> pricesPerNight = packageMapper.fromRequests(requests);
+
+    assertEquals(expectedPrice, pricesPerNight.get(packageName));
+  }
+
+  @Test
+  public void fromRequests_withMultipleRequests_shouldMapPricesPerNight() {
+    double priceValue = 100.0;
+    double otherPriceValue = 200.0;
+    Price expectedPrice = new Price(BigDecimal.valueOf(priceValue));
+    Price otherExpectedPrice = new Price(BigDecimal.valueOf(otherPriceValue));
+    PackageNames packageName = PackageNames.BLOODTHIRSTY;
+    PackageNames otherPackageName = PackageNames.SWEET_TOOTH;
+    PackageRequest request =
+        aPackageRequest().withName(packageName.toString()).withPricePerNight(priceValue).build();
+    PackageRequest otherRequest =
+        aPackageRequest()
+            .withName(otherPackageName.toString())
+            .withPricePerNight(otherPriceValue)
+            .build();
+    List<PackageRequest> requests = Arrays.asList(request, otherRequest);
+
+    Map<PackageNames, Price> pricesPerNight = packageMapper.fromRequests(requests);
+
+    assertEquals(expectedPrice, pricesPerNight.get(packageName));
+    assertEquals(otherExpectedPrice, pricesPerNight.get(otherPackageName));
+  }
+
+  // TODO : Update toResponse tests
 
   @Test
   public void toResponse_shouldMapName() {
