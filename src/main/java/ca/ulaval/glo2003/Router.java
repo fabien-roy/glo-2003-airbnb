a3daa2896ef790eccbd0a8ab71b1effd76a4f5f5
bigpkd <bigpkd@yahoo.fr>
2020-02-29 11:21:56 -0500

    [RUS] Add BookingResource

--- src/main/java/ca/ulaval/glo2003/Router.java
+++ src/main/java/ca/ulaval/glo2003/Router.java
@@ -1,15 +1,19 @@
 package ca.ulaval.glo2003;
 
-import static ca.ulaval.glo2003.beds.rest.BedResource.BED_PATH;
+import static ca.ulaval.glo2003.beds.bookings.rest.BookingResource.BOOKING_PATH;
 import static ca.ulaval.glo2003.interfaces.rest.ErrorResource.ERROR_PATH;
 import static ca.ulaval.glo2003.interfaces.rest.RootResource.ROOT_PATH;
 import static spark.Spark.path;
 
+import ca.ulaval.glo2003.beds.bookings.rest.BookingResource;
+import ca.ulaval.glo2003.beds.bookings.rest.mappers.BookingMapper;
+import ca.ulaval.glo2003.beds.bookings.rest.mappers.BookingNumberMapper;
+import ca.ulaval.glo2003.beds.bookings.services.BookingService;
+import ca.ulaval.glo2003.beds.bookings.transactions.domain.TransactionFactory;
 import ca.ulaval.glo2003.beds.domain.BedFactory;
 import ca.ulaval.glo2003.beds.domain.BedRepository;
 import ca.ulaval.glo2003.beds.domain.BedStarsCalculator;
 import ca.ulaval.glo2003.beds.infrastructure.InMemoryBedRepository;
-import ca.ulaval.glo2003.beds.rest.BedResource;
 import ca.ulaval.glo2003.beds.rest.mappers.BedMapper;
 import ca.ulaval.glo2003.beds.rest.mappers.BedMatcherMapper;
 import ca.ulaval.glo2003.beds.rest.mappers.BedNumberMapper;
@@ -24,6 +28,7 @@ public class Router {
   private static final BedRepository bedRepository = new InMemoryBedRepository();
   private static final BedStarsCalculator bedStarsCalculator = new BedStarsCalculator();
   private static final BedFactory bedFactory = new BedFactory();
+  private static final TransactionFactory transactionFactory = new TransactionFactory();
   private static final PackageMapper packageMapper = new PackageMapper();
   private static final BedMapper bedMapper = new BedMapper(packageMapper);
   private static final BedNumberMapper bedNumberMapper = new BedNumberMapper();
@@ -36,10 +41,15 @@ public class Router {
           bedMatcherMapper,
           bedRepository,
           bedStarsCalculator);
+  private static final BookingMapper bookingMapper = new BookingMapper();
+  private static final BookingNumberMapper bookingNumberMapper = new BookingNumberMapper();
+  private static final BookingService bookingService =
+      new BookingService(
+          transactionFactory, bedRepository, bookingMapper, bedNumberMapper, bookingNumberMapper);
 
   public static void setUpRoutes() {
     path(ERROR_PATH, new ErrorResource());
     path(ROOT_PATH, new RootResource());
-    path(BED_PATH, new BedResource(bedService));
+    path(BOOKING_PATH, new BookingResource(bookingService));
   }
 }
