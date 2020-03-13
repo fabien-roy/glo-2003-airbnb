9aa1bfc322d109bdc97a2fdcc6e9efbf47e53a32
vince2848 <vince2848@msn.com>
2020-03-13 09:03:11 -0400

    [FLG] Add validateValidZipCode method

--- src/main/java/ca/ulaval/glo2003/interfaces/clients/ZippopotamusClient.java
+++ src/main/java/ca/ulaval/glo2003/interfaces/clients/ZippopotamusClient.java
@@ -1,3 +1,23 @@
 package ca.ulaval.glo2003.interfaces.clients;
 
-public class ZippopotamusClient {}
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ZippopotamusClient {
+
+  public ZippopotamusClient() {}
+
+  public static void validateZipCode(String zipCode) {
+    String host = "http://api.zippopotam.us/us/";
+    try {
+      URL urlForGetRequest = new URL(host + zipCode);
+      String readLine = null;
+      HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
+      conection.setRequestMethod("GET");
+      int responseCode = conection.getResponseCode();
+    } catch (IOException ex) {
+      // TODO
+    }
+  }
+}
