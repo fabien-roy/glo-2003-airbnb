3dd5d35a4ea773a894dbe974102c9f874dbce293
vince2848 <vince2848@msn.com>
2020-03-13 08:54:44 -0400

    [FLG] Add a test to validateValidZipCode no throw

--- src/test/java/ca/ulaval/glo2003/interfaces/clients/ZippopotamusClientTest.java
+++ src/test/java/ca/ulaval/glo2003/interfaces/clients/ZippopotamusClientTest.java
@@ -1,5 +1,15 @@
 package ca.ulaval.glo2003.interfaces.clients;
 
+import static ca.ulaval.glo2003.interfaces.clients.ZippopotamusClient.validateZipCode;
 import static org.junit.jupiter.api.Assertions.*;
 
-class ZippopotamusClientTest {}
+import org.junit.jupiter.api.Test;
+
+class ZippopotamusClientTest {
+
+  @Test
+  void validateZipCode_withValidZipCode_shouldNotThrow() {
+    String zipCode = "12345";
+    assertDoesNotThrow(() -> validateZipCode(zipCode));
+  }
+}
