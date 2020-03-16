f66801c3b11ba8c20587cf0dcf0c30bf71b8f896
Fabien Roy <fabienroy28@gmail.com>
2020-03-16 11:58:03 -0400

    [HOTFIX] Remove RootResource

--- src/main/java/ca/ulaval/glo2003/Router.java
+++ src/main/java/ca/ulaval/glo2003/Router.java
@@ -3,7 +3,6 @@ package ca.ulaval.glo2003;
 import static ca.ulaval.glo2003.beds.bookings.rest.BookingResource.BOOKING_PATH;
 import static ca.ulaval.glo2003.beds.bookings.transactions.rest.TransactionResource.TRANSACTION_PATH;
 import static ca.ulaval.glo2003.beds.rest.BedResource.BED_PATH;
-import static ca.ulaval.glo2003.interfaces.rest.RootResource.ROOT_PATH;
 import static ca.ulaval.glo2003.interfaces.rest.mappers.ErrorMapper.ERROR_PATH;
 import static spark.Spark.path;
 
@@ -24,7 +23,6 @@ import ca.ulaval.glo2003.beds.infrastructure.InMemoryBedRepository;
 import ca.ulaval.glo2003.beds.rest.BedResource;
 import ca.ulaval.glo2003.beds.rest.mappers.*;
 import ca.ulaval.glo2003.beds.services.BedService;
-import ca.ulaval.glo2003.interfaces.rest.RootResource;
 import ca.ulaval.glo2003.interfaces.rest.mappers.ErrorMapper;
 
 public class Router {
@@ -72,7 +70,6 @@ public class Router {
 
   public static void setUpRoutes() {
     path(ERROR_PATH, new ErrorMapper());
-    path(ROOT_PATH, new RootResource());
     path(BED_PATH, new BedResource(bedService));
     path(BOOKING_PATH, new BookingResource(bookingService));
     path(TRANSACTION_PATH, new TransactionResource(transactionService));
