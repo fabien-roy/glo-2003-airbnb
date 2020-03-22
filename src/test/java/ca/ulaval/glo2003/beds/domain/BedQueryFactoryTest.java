0d807a9ad67dc4f1b3d0c5b4f257b14f49261a10
Fabien Roy <fabienroy28@gmail.com>
2020-03-22 18:15:37 -0400

    [IMPROV] Fix tests

--- src/test/java/ca/ulaval/glo2003/beds/domain/BedQueryFactoryTest.java
+++ src/test/java/ca/ulaval/glo2003/beds/domain/BedQueryFactoryTest.java
@@ -14,30 +14,22 @@ class BedQueryFactoryTest {
 
   private static BedQueryFactory bedQueryFactory;
   private static BedQueryBuilder bedQueryBuilder = mock(BedQueryBuilder.class);
-  private static BedQueryBuilder firstBedQueryBuilder = mock(BedQueryBuilder.class);
-  private static BedQueryBuilder secondBedQueryBuilder = mock(BedQueryBuilder.class);
+  private static BedQueryBuilder filteredBedQueryBuilder = mock(BedQueryBuilder.class);
 
-  private BedTypeQueryParamAssembler queryAssembler = mock(BedTypeQueryParamAssembler.class);
-  private BedTypeQueryParamAssembler otherQueryAssembler = mock(BedTypeQueryParamAssembler.class);
+  private BedTypeQueryParamAssembler firstQueryAssembler = mock(BedTypeQueryParamAssembler.class);
   private InMemoryBedQuery query = mock(InMemoryBedQuery.class);
   private InMemoryBedQuery filteredQuery = mock(InMemoryBedQuery.class);
-  private InMemoryBedQuery otherFilteredQuery = mock(InMemoryBedQuery.class);
   private Map<String, String[]> params = new HashMap<>();
 
   private Set<BedQueryParamAssembler> queryParamAssemblers =
-      new HashSet<>(Collections.singletonList(queryAssembler));
-  private Set<BedQueryParamAssembler> otherQueryParamAssemblers =
-      new HashSet<>(Arrays.asList(queryAssembler, otherQueryAssembler));
+      new HashSet<>(Collections.singletonList(firstQueryAssembler));
 
   @BeforeEach
   public void setUpMocks() {
     when(bedQueryBuilder.aBedQuery()).thenReturn(bedQueryBuilder);
     when(bedQueryBuilder.build()).thenReturn(query);
-    when(firstBedQueryBuilder.build()).thenReturn(filteredQuery);
-    when(secondBedQueryBuilder.build()).thenReturn(otherFilteredQuery);
-    when(queryAssembler.assemble(bedQueryBuilder, params)).thenReturn(firstBedQueryBuilder);
-    when(otherQueryAssembler.assemble(firstBedQueryBuilder, params))
-        .thenReturn(secondBedQueryBuilder);
+    when(filteredBedQueryBuilder.build()).thenReturn(filteredQuery);
+    when(firstQueryAssembler.assemble(bedQueryBuilder, params)).thenReturn(filteredBedQueryBuilder);
   }
 
   @Test
@@ -50,20 +42,11 @@ class BedQueryFactoryTest {
   }
 
   @Test
-  public void create_withSingleAssembler_shouldCreateQuery() {
+  public void create_withAssemblers_shouldCreateQuery() {
     bedQueryFactory = new BedQueryFactory(bedQueryBuilder, queryParamAssemblers);
 
     InMemoryBedQuery actualQuery = bedQueryFactory.create(params);
 
     assertSame(filteredQuery, actualQuery);
-  }
-
-  @Test
-  public void create_withMultipleAssemblers_shouldCreateQuery() {
-    bedQueryFactory = new BedQueryFactory(bedQueryBuilder, otherQueryParamAssemblers);
-
-    InMemoryBedQuery actualQuery = bedQueryFactory.create(params);
-
-    assertSame(otherFilteredQuery, actualQuery);
   }
 }
--- src/test/java/ca/ulaval/glo2003/beds/infrastructure/InMemoryBedQueryTest.java
+++ src/test/java/ca/ulaval/glo2003/beds/infrastructure/InMemoryBedQueryTest.java
@@ -10,48 +10,38 @@ import ca.ulaval.glo2003.beds.infrastructure.filters.InMemoryBedFilter;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class InMemoryBedQueryTest {
 
-  private InMemoryBedFilter filter = mock(InMemoryBedFilter.class);
-  private InMemoryBedFilter otherFilter = mock(InMemoryBedFilter.class);
+  private static InMemoryBedQuery query;
+  private static InMemoryBedFilter filter = mock(InMemoryBedFilter.class);
   private List<Bed> beds;
   private List<Bed> filteredBeds;
-  private List<Bed> otherFilteredBeds;
+
+  @BeforeAll
+  public static void setUpQuery() {
+    query = new InMemoryBedQuery(Collections.singletonList(filter));
+  }
 
   @BeforeEach
   public void setUpMocks() {
     Bed bed = aBed().build();
     Bed filteredBed = aBed().build();
-    Bed otherFilteredBed = aBed().build();
-    beds = Arrays.asList(bed, filteredBed, otherFilteredBed);
-    filteredBeds = Arrays.asList(filteredBed, otherFilteredBed);
-    otherFilteredBeds = Collections.singletonList(otherFilteredBed);
+    beds = Arrays.asList(bed, filteredBed);
+    filteredBeds = Collections.singletonList(filteredBed);
     when(filter.filter(beds)).thenReturn(filteredBeds);
-    when(otherFilter.filter(filteredBeds)).thenReturn(otherFilteredBeds);
   }
 
   @Test
   public void filter_withQuery_shouldFilterWithQuery() {
-    InMemoryBedQuery query = new InMemoryBedQuery(Collections.singletonList(filter));
     query.setBeds(beds);
 
     List<Bed> actualBeds = query.execute();
 
     assertEquals(filteredBeds.size(), actualBeds.size());
     assertTrue(filteredBeds.containsAll(actualBeds));
-  }
-
-  @Test
-  public void filter_withMultipleQueries_shouldFilterWithQueries() {
-    InMemoryBedQuery query = new InMemoryBedQuery(Arrays.asList(filter, otherFilter));
-    query.setBeds(beds);
-
-    List<Bed> actualBeds = query.execute();
-
-    assertEquals(otherFilteredBeds.size(), actualBeds.size());
-    assertTrue(otherFilteredBeds.containsAll(actualBeds));
   }
 }
