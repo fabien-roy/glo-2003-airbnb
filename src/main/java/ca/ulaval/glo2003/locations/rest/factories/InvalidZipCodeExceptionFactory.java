ac68d062474d49ce2a2a97c0a518f4df7f049ff8
vince2848 <vince2848@msn.com>
2020-03-23 12:06:56 -0400

    [IMPROV] Implement InvalidZipCodeExceptionFactory

---
+++ src/main/java/ca/ulaval/glo2003/locations/rest/factories/InvalidZipCodeExceptionFactory.java
@@ -0,0 +1,25 @@
+package ca.ulaval.glo2003.locations.rest.factories;
+
+import static ca.ulaval.glo2003.errors.ErrorFactory.tryWriteValueAsString;
+
+import ca.ulaval.glo2003.errors.ErrorFactory;
+import ca.ulaval.glo2003.locations.exceptions.InvalidZipCodeException;
+import org.eclipse.jetty.http.HttpStatus;
+
+public class InvalidZipCodeExceptionFactory implements ErrorFactory {
+
+  @Override
+  public boolean canHandle(Exception exception) {
+    return exception instanceof InvalidZipCodeException;
+  }
+
+  @Override
+  public String createResponse() {
+    return tryWriteValueAsString("INVALID_ZIP_CODE", "zip code should be a 5 digits number");
+  }
+
+  @Override
+  public int createStatus() {
+    return HttpStatus.BAD_REQUEST_400;
+  }
+}
