7945da7012dc2c83a2bf72312ada704e8fddc3e5
vince2848 <vince2848@msn.com>
2020-03-23 22:03:34 -0400

    [IMPROV] Implement CatchallExceptionHandler

--- src/main/java/ca/ulaval/glo2003/errors/rest/handlers/CatchallExceptionHandler.java
+++ src/main/java/ca/ulaval/glo2003/errors/rest/handlers/CatchallExceptionHandler.java
@@ -1,28 +1,56 @@
 package ca.ulaval.glo2003.errors.rest.handlers;
 
-import ca.ulaval.glo2003.errors.rest.factories.CatchallErrorResponseFactory;
-import ca.ulaval.glo2003.errors.rest.factories.CatchallErrorStatusFactory;
+import static ca.ulaval.glo2003.errors.ErrorFactory.tryWriteValueAsString;
+
+import ca.ulaval.glo2003.errors.ErrorFactory;
+import ca.ulaval.glo2003.errors.rest.factories.InvalidFormatExceptionFactory;
+import ca.ulaval.glo2003.errors.rest.factories.JsonProcessingExceptionFactory;
+import com.google.inject.multibindings.Multibinder;
+import java.util.List;
+import java.util.Optional;
 import javax.inject.Inject;
+import org.eclipse.jetty.http.HttpStatus;
 import spark.ExceptionHandler;
 import spark.Request;
 import spark.Response;
 
 public class CatchallExceptionHandler implements ExceptionHandler<Exception> {
 
-  private final CatchallErrorStatusFactory catchallErrorStatusFactory;
-  private final CatchallErrorResponseFactory catchallErrorResponseFactory;
+  private final Multibinder<ErrorFactory> factories;
 
   @Inject
-  public CatchallExceptionHandler(
-      CatchallErrorStatusFactory catchallErrorStatusFactory,
-      CatchallErrorResponseFactory catchallErrorResponseFactory) {
-    this.catchallErrorStatusFactory = catchallErrorStatusFactory;
-    this.catchallErrorResponseFactory = catchallErrorResponseFactory;
+  public CatchallExceptionHandler(Multibinder<ErrorFactory> factories) {
+    this.factories = factories;
+    factories.addBinding().to(InvalidFormatExceptionFactory.class);
+    factories.addBinding().to(JsonProcessingExceptionFactory.class);
   }
 
   @Override
   public void handle(Exception e, Request request, Response response) {
-    response.status(catchallErrorStatusFactory.create(e));
-    response.body(catchallErrorResponseFactory.create(e));
+    List<ErrorFactory> factories = null;
+    String errorResponse;
+    int status;
+
+    Optional<ErrorFactory> foundFactory =
+        factories.stream().filter(factory -> factory.canHandle(e)).findFirst();
+
+    if (foundFactory.isPresent()) {
+      errorResponse = foundFactory.get().createResponse();
+      status = foundFactory.get().createStatus();
+    } else {
+      status = defaultStatus();
+      errorResponse = defaultBody();
+    }
+
+    response.status(status);
+    response.body(errorResponse);
+  }
+
+  private static int defaultStatus() {
+    return HttpStatus.BAD_REQUEST_400;
+  }
+
+  private static String defaultBody() {
+    return tryWriteValueAsString("BAD_REQUEST", "could not parse JSON");
   }
 }
