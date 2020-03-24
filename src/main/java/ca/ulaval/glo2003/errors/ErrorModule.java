4a025894f48e6fd322a700e977d1654a679b040e
vince2848 <vince2848@msn.com>
2020-03-24 14:51:00 -0400

    [IMPROV] Add configure ErrorFactories

--- src/main/java/ca/ulaval/glo2003/errors/ErrorModule.java
+++ src/main/java/ca/ulaval/glo2003/errors/ErrorModule.java
@@ -2,19 +2,31 @@ package ca.ulaval.glo2003.errors;
 
 import ca.ulaval.glo2003.beds.rest.handlers.BedExceptionHandler;
 import ca.ulaval.glo2003.bookings.rest.handlers.BookingExceptionHandler;
+import ca.ulaval.glo2003.errors.rest.factories.*;
 import ca.ulaval.glo2003.errors.rest.handlers.CatchallExceptionHandler;
 import ca.ulaval.glo2003.errors.rest.mappers.ErrorMapper;
 import ca.ulaval.glo2003.locations.rest.handlers.LocationServiceExceptionHandler;
 import com.google.inject.AbstractModule;
+import com.google.inject.TypeLiteral;
+import com.google.inject.multibindings.Multibinder;
 
 public class ErrorModule extends AbstractModule {
 
   @Override
   protected void configure() {
+    configureErrorFactories();
+
     bind(CatchallExceptionHandler.class);
     bind(LocationServiceExceptionHandler.class);
     bind(BedExceptionHandler.class);
     bind(BookingExceptionHandler.class);
     bind(ErrorMapper.class);
+  }
+
+  private void configureErrorFactories() {
+    Multibinder<CatchallErrorFactory> multibinder =
+        Multibinder.newSetBinder(binder(), new TypeLiteral<CatchallErrorFactory>() {});
+    multibinder.addBinding().to(InvalidFormatErrorFactory.class);
+    multibinder.addBinding().to(JsonProcessingErrorFactory.class);
   }
 }
