00f2a6ba05991686a6399fc5714d90a90c30f3fc
vince2848 <vince2848@msn.com>
2020-03-24 14:36:42 -0400

    [IMPROV] Implement abstract BedErrorFactory

---
+++ src/main/java/ca/ulaval/glo2003/beds/rest/factories/BedErrorFactory.java
@@ -0,0 +1,6 @@
+package ca.ulaval.glo2003.beds.rest.factories;
+
+import ca.ulaval.glo2003.beds.exceptions.BedException;
+import ca.ulaval.glo2003.errors.rest.factories.ErrorFactory;
+
+public abstract class BedErrorFactory implements ErrorFactory<BedException> {}
