b664b8de29735b9bfbad1016f3d51f50bec7b5e7
vince2848 <vince2848@msn.com>
2020-03-24 14:45:10 -0400

    [IMPROV] Implement abstract LocationServiceErrorFactory

---
+++ src/main/java/ca/ulaval/glo2003/locations/rest/factories/LocationServiceErrorFactory.java
@@ -0,0 +1,7 @@
+package ca.ulaval.glo2003.locations.rest.factories;
+
+import ca.ulaval.glo2003.errors.rest.factories.ErrorFactory;
+import ca.ulaval.glo2003.locations.exceptions.LocationServiceException;
+
+public abstract class LocationServiceErrorFactory
+    implements ErrorFactory<LocationServiceException> {}
