9e7c8a0655721338ebad04e8661285b9a5a96ea5
vince2848 <vince2848@msn.com>
2020-03-24 14:37:21 -0400

    [IMPROV] Im̀plement abstract BookingErrorFactory

---
+++ src/main/java/ca/ulaval/glo2003/bookings/rest/factories/BookingErrorFactory.java
@@ -0,0 +1,6 @@
+package ca.ulaval.glo2003.bookings.rest.factories;
+
+import ca.ulaval.glo2003.bookings.exceptions.BookingException;
+import ca.ulaval.glo2003.errors.rest.factories.ErrorFactory;
+
+public abstract class BookingErrorFactory implements ErrorFactory<BookingException> {}
