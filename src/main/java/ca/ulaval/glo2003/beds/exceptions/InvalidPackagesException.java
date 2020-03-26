dbaa0949c26719bb10530e43c966d3de06b28663
Fabien Roy <fabienroy28@gmail.com>
2020-03-26 12:49:04 -0400

    [HOTFIX] Typo

--- src/main/java/ca/ulaval/glo2003/beds/exceptions/InvalidPackagesException.java
+++ src/main/java/ca/ulaval/glo2003/beds/exceptions/InvalidPackagesException.java
@@ -3,6 +3,6 @@ package ca.ulaval.glo2003.beds.exceptions;
 public class InvalidPackagesException extends BedException {
 
   public InvalidPackagesException() {
-    super("INVALID_PACKAGE");
+    super("INVALID_PACKAGES");
   }
 }
