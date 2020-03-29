e395dc3bf99de29ed77622694c40919b7f99a576
Fabien Roy <fabienroy28@gmail.com>
2020-03-29 15:54:38 -0400

    [HOTFIX] Check zipCode not null

--- src/main/java/ca/ulaval/glo2003/locations/converters/ZipCodeConverter.java
+++ src/main/java/ca/ulaval/glo2003/locations/converters/ZipCodeConverter.java
@@ -6,13 +6,13 @@ import ca.ulaval.glo2003.locations.exceptions.InvalidZipCodeException;
 public class ZipCodeConverter {
 
   public ZipCode fromString(String value) {
-    validateLength(value);
+    validateString(value);
     validateNumeric(value);
     return new ZipCode(value);
   }
 
-  private void validateLength(String value) {
-    if (value.length() != 5) {
+  private void validateString(String value) {
+    if (value == null || value.length() != 5) {
       throw new InvalidZipCodeException();
     }
   }
