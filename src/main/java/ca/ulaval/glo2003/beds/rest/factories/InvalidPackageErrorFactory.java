17702c8c8a16cccf4e6a2b0968a590f14e11b2d3
Fabien Roy <fabienroy28@gmail.com>
2020-03-29 16:23:32 -0400

    [HOTFIX] Fix packages errors even more

--- src/main/java/ca/ulaval/glo2003/beds/rest/factories/InvalidPackageErrorFactory.java
+++ src/main/java/ca/ulaval/glo2003/beds/rest/factories/InvalidPackageErrorFactory.java
@@ -1,14 +1,14 @@
 package ca.ulaval.glo2003.beds.rest.factories;
 
 import ca.ulaval.glo2003.beds.exceptions.BedException;
-import ca.ulaval.glo2003.beds.exceptions.InvalidPackagesException;
+import ca.ulaval.glo2003.beds.exceptions.InvalidPackageException;
 import org.eclipse.jetty.http.HttpStatus;
 
 public class InvalidPackageErrorFactory extends BedErrorFactory {
 
   @Override
   public boolean canHandle(BedException exception) {
-    return exception instanceof InvalidPackagesException;
+    return exception instanceof InvalidPackageException;
   }
 
   @Override
