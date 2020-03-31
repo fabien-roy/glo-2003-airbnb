d70bd4dce96b24449ee63f448ffad35b1beb3c99
vince2848 <vince2848@msn.com>
2020-03-31 14:27:44 -0400

    [AUL] Remove Capacity validation from BedFactory

--- src/main/java/ca/ulaval/glo2003/beds/domain/BedFactory.java
+++ src/main/java/ca/ulaval/glo2003/beds/domain/BedFactory.java
@@ -1,24 +1,14 @@
 package ca.ulaval.glo2003.beds.domain;
 
-import ca.ulaval.glo2003.beds.exceptions.ExceedingAccommodationCapacityException;
 import ca.ulaval.glo2003.locations.domain.Location;
 import java.util.UUID;
 
 public class BedFactory {
 
   public Bed create(Bed bed, Location location) {
-    validateCapacity(bed);
     UUID bedNumber = UUID.randomUUID();
     bed.setNumber(bedNumber);
     bed.setLocation(location);
     return bed;
-  }
-
-  private void validateCapacity(Bed bed) {
-    int maxCapacity = BedTypesCapacities.get(bed.getBedType());
-
-    if (bed.getCapacity() > maxCapacity) {
-      throw new ExceedingAccommodationCapacityException();
-    }
   }
 }
