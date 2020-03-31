e90cfcb258898e7708d8ea63710cd7987ac80f56
Hamza Ben Kirane <hamza.benkirane.1@ulaval.ca>
2020-03-31 08:33:28 +0200

    Added CalculateOwner/TenantRefund

--- src/main/java/ca/ulaval/glo2003/bookings/domain/CancelationRefundCalculator.java
+++ src/main/java/ca/ulaval/glo2003/bookings/domain/CancelationRefundCalculator.java
@@ -2,16 +2,19 @@ package ca.ulaval.glo2003.bookings.domain;
 
 import ca.ulaval.glo2003.transactions.domain.Price;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CancelationRefundCalculator {
 
   public Price calculateOwnerRefund(Price total) {
-    // TODO
-    return new Price(BigDecimal.ZERO);
+    Price halfOwnerPrice =
+        new Price(total.getValue().divide(BigDecimal.valueOf(2), RoundingMode.UP));
+    return halfOwnerPrice;
   }
 
   public Price calculateTenantRefund(Price total) {
-    // TODO
-    return new Price(BigDecimal.ZERO);
+    Price halfTenantPrice =
+        new Price(total.getValue().divide(BigDecimal.valueOf(2), RoundingMode.DOWN));
+    return halfTenantPrice;
   }
 }
--- src/test/java/ca/ulaval/glo2003/bookings/domain/CancelationRefundCalculatorTest.java
+++ src/test/java/ca/ulaval/glo2003/bookings/domain/CancelationRefundCalculatorTest.java
@@ -1,50 +1,53 @@
 package ca.ulaval.glo2003.bookings.domain;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import ca.ulaval.glo2003.transactions.domain.Price;
+import java.math.BigDecimal;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
-
-import static org.mockito.Mockito.*;
-
-
-import static org.junit.jupiter.api.Assertions.*;
-
 public class CancelationRefundCalculatorTest {
 
-    private static CancelationRefundCalculator cancelationRefundCalculator;
+  private static CancelationRefundCalculator cancelationRefundCalculator;
+  private static Price total;
 
-    @BeforeAll
-    public static void setUpService() {
-        cancelationRefundCalculator = new CancelationRefundCalculator();
-    }
+  @BeforeAll
+  public static void setUpService() {
+    cancelationRefundCalculator = new CancelationRefundCalculator();
+    total = mock(Price.class);
+  }
 
-    @Test
-    public void calculateOwnerRefund_shouldReturnHalfOfTotal() {
-        Price total = mock(Price.class);
-        when(total.getValue()).thenReturn(BigDecimal.valueOf(20.00));
-        assertEquals(BigDecimal.valueOf(10.00),cancelationRefundCalculator.calculateOwnerRefund(total));
-    }
+  @Test
+  public void calculateOwnerRefund_shouldReturnHalfOfTotal() {
+    when(total.getValue()).thenReturn(BigDecimal.valueOf(20.00));
+    assertEquals(
+        BigDecimal.valueOf(10.00),
+        cancelationRefundCalculator.calculateOwnerRefund(total).getValue());
+  }
 
-    @Test
-    public void calculateOwnerRefund_shouldReturnHigherHalfOfTotal() {
-        Price total = mock(Price.class);
-        when(total.getValue()).thenReturn(BigDecimal.valueOf(40.11));
-        assertEquals(BigDecimal.valueOf(20.06),cancelationRefundCalculator.calculateOwnerRefund(total));
-    }
+  @Test
+  public void calculateOwnerRefund_shouldReturnHigherHalfOfTotal() {
+    when(total.getValue()).thenReturn(BigDecimal.valueOf(40.11));
+    assertEquals(
+        BigDecimal.valueOf(20.06),
+        cancelationRefundCalculator.calculateOwnerRefund(total).getValue());
+  }
 
-    @Test
-    public void calculateTenantRefund_shouldReturnHalfOfTotal() {
-        Price total = mock(Price.class);
-        when(total.getValue()).thenReturn(BigDecimal.valueOf(20.00));
-        assertEquals(BigDecimal.valueOf(10.00),cancelationRefundCalculator.calculateOwnerRefund(total));
-    }
+  @Test
+  public void calculateTenantRefund_shouldReturnHalfOfTotal() {
+    when(total.getValue()).thenReturn(BigDecimal.valueOf(20.00));
+    assertEquals(
+        BigDecimal.valueOf(10.00),
+        cancelationRefundCalculator.calculateTenantRefund(total).getValue());
+  }
 
-    @Test
-    public void calculateTenantRefund_shouldReturnLowerHalfOfTotal() {
-        Price total = mock(Price.class);
-        when(total.getValue()).thenReturn(BigDecimal.valueOf(40.11));
-        assertEquals(BigDecimal.valueOf(20.05),cancelationRefundCalculator.calculateOwnerRefund(total));
-    }
+  @Test
+  public void calculateTenantRefund_shouldReturnLowerHalfOfTotal() {
+    when(total.getValue()).thenReturn(BigDecimal.valueOf(40.11));
+    assertEquals(
+        BigDecimal.valueOf(20.05),
+        cancelationRefundCalculator.calculateTenantRefund(total).getValue());
+  }
 }
