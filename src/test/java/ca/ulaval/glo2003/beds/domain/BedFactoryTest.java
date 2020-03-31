9095cca94b3fbdac1621bf196128f24ca717cfa3
vince2848 <vince2848@msn.com>
2020-03-31 16:20:05 -0400

    [AUL] Remove bedCapacity test from BedFactoryTest

--- src/test/java/ca/ulaval/glo2003/beds/domain/BedFactoryTest.java
+++ src/test/java/ca/ulaval/glo2003/beds/domain/BedFactoryTest.java
@@ -4,12 +4,9 @@ import static ca.ulaval.glo2003.beds.domain.helpers.BedBuilder.aBed;
 import static ca.ulaval.glo2003.beds.domain.helpers.BedObjectMother.createLocation;
 import static org.junit.jupiter.api.Assertions.*;
 
-import ca.ulaval.glo2003.beds.exceptions.ExceedingAccommodationCapacityException;
 import ca.ulaval.glo2003.locations.domain.Location;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
 
 class BedFactoryTest {
 
@@ -50,16 +47,5 @@ class BedFactoryTest {
     otherBed = bedFactory.create(otherBed, location);
 
     assertNotEquals(bed.getNumber(), otherBed.getNumber());
-  }
-
-  @ParameterizedTest
-  @EnumSource(BedTypes.class)
-  public void create_withExceedingCapacity_shouldThrowExceedingAccommodationCapacityException(
-      BedTypes bedType) {
-    int maxCapacity = BedTypesCapacities.get(bedType);
-    bed = aBed().withBedType(bedType).withCapacity(maxCapacity + 1).build();
-
-    assertThrows(
-        ExceedingAccommodationCapacityException.class, () -> bedFactory.create(bed, location));
   }
 }
