f0d055b6c37dacfd57671c84d336ecf2104adead
Sandrine Diffo <sandrinediffo@Sandrines-MacBook-Pro.local>
2020-04-02 15:56:25 -0400

    Update BookingTotalCalculation With ColonySize

--- src/test/java/ca/ulaval/glo2003/bookings/domain/BookingTotalCalculatorTest.java
+++ src/test/java/ca/ulaval/glo2003/bookings/domain/BookingTotalCalculatorTest.java
@@ -19,14 +19,19 @@ import org.junit.jupiter.params.provider.MethodSource;
 class BookingTotalCalculatorTest {
 
   @ParameterizedTest
-  @MethodSource("provideConditionsForCalculateTotal")
-  public void calculateTotal_shouldReturnCorrectTotal(
+  @MethodSource("provideConditionsForCalculateTotalWithoutColonySize")
+  public void calculateTotal_shouldReturnCorrectTotalWithoutColonySize(
       Price pricePerNight, int numberOfNights, Price expectedTotal) {
     Packages packageName = createPackageName();
+    Integer colonySize = null;
     Map<Packages, Price> pricesPerNight = Collections.singletonMap(packageName, pricePerNight);
     Bed bed = aBed().withPricesPerNights(pricesPerNight).build();
     Booking booking =
-        aBooking().withPackage(packageName).withNumberOfNights(numberOfNights).build();
+        aBooking()
+            .withPackage(packageName)
+            .withNumberOfNights(numberOfNights)
+            .withColonySize(colonySize)
+            .build();
     BookingTotalCalculator bookingTotalCalculator = new BookingTotalCalculator();
 
     Price total = bookingTotalCalculator.calculateTotal(bed, booking);
@@ -34,7 +39,7 @@ class BookingTotalCalculatorTest {
     assertEquals(expectedTotal, total);
   }
 
-  private static Stream<Arguments> provideConditionsForCalculateTotal() {
+  private static Stream<Arguments> provideConditionsForCalculateTotalWithoutColonySize() {
     return Stream.of(
         Arguments.of(
             new Price(BigDecimal.valueOf(100)), 1, new Price(BigDecimal.valueOf(100))), // 100 * 1
@@ -50,5 +55,57 @@ class BookingTotalCalculatorTest {
             new Price(BigDecimal.valueOf(100)),
             30,
             new Price(BigDecimal.valueOf(2250)))); // 100 * 30 * 0.75
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideConditionsForCalculateTotalWithColonySize")
+  public void calculateTotal_shouldReturnCorrectTotalWithColonySize(
+      Price pricePerNight,
+      int numberOfNights,
+      Integer colonySize,
+      int bedCapacity,
+      Price expectedTotal) {
+    Packages packageName = createPackageName();
+    Map<Packages, Price> pricesPerNight = Collections.singletonMap(packageName, pricePerNight);
+    Bed bed = aBed().withPricesPerNights(pricesPerNight).withCapacity(bedCapacity).build();
+    Booking booking =
+        aBooking()
+            .withPackage(packageName)
+            .withNumberOfNights(numberOfNights)
+            .withColonySize(colonySize)
+            .build();
+    BookingTotalCalculator bookingTotalCalculator = new BookingTotalCalculator();
+
+    Price total = bookingTotalCalculator.calculateTotal(bed, booking);
+
+    assertEquals(expectedTotal, total);
+  }
+
+  private static Stream<Arguments> provideConditionsForCalculateTotalWithColonySize() {
+    return Stream.of(
+        Arguments.of(
+            new Price(BigDecimal.valueOf(100)),
+            1,
+            10,
+            20,
+            new Price(BigDecimal.valueOf(50))), // 100 * 1
+        Arguments.of(
+            new Price(BigDecimal.valueOf(100)),
+            3,
+            10,
+            20,
+            new Price(BigDecimal.valueOf(142.5))), // 100 * 3 * 0.95
+        Arguments.of(
+            new Price(BigDecimal.valueOf(100)),
+            10,
+            10,
+            20,
+            new Price(BigDecimal.valueOf(450))), // 100 * 10 * 0.9
+        Arguments.of(
+            new Price(BigDecimal.valueOf(100)),
+            30,
+            10,
+            20,
+            new Price(BigDecimal.valueOf(1125)))); // 100 * 30 * 0.75
   }
 }
