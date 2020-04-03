eb6d5285b9c9613925a18ee0012ac6bf55157ed5
Fabien Roy <fabienroy28@gmail.com>
2020-04-03 13:55:09 -0400

    [AUL] Update exception

--- src/main/java/ca/ulaval/glo2003/beds/converters/PublicKeyConverter.java
+++ src/main/java/ca/ulaval/glo2003/beds/converters/PublicKeyConverter.java
@@ -1,7 +1,6 @@
 package ca.ulaval.glo2003.beds.converters;
 
 import ca.ulaval.glo2003.beds.domain.PublicKey;
-import ca.ulaval.glo2003.beds.exceptions.InvalidBloodTypesException;
 import ca.ulaval.glo2003.beds.exceptions.InvalidPublicKeyException;
 
 public class PublicKeyConverter {
@@ -9,7 +8,7 @@ public class PublicKeyConverter {
   public static final String PUBLIC_KEY_PATTERN = "([A-F]|[0-9]){64}";
 
   public PublicKey fromString(String value) {
-    if (value == null) throw new InvalidBloodTypesException();
+    if (value == null) throw new InvalidPublicKeyException();
 
     String upperCaseValue = value.toUpperCase();
 
