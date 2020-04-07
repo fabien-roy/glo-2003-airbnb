ba15f7283cb2711506bca7482fb9f8af50097f72
Fabien Roy <fabienroy28@gmail.com>
2020-04-07 17:31:49 -0400

    [AUR] Update cancelation response attribute

--- src/main/java/ca/ulaval/glo2003/bookings/rest/CancelationResponse.java
+++ src/main/java/ca/ulaval/glo2003/bookings/rest/CancelationResponse.java
@@ -1,12 +1,10 @@
 package ca.ulaval.glo2003.bookings.rest;
 
 import ca.ulaval.glo2003.transactions.rest.serializers.PriceSerializer;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 public class CancelationResponse {
 
-  @JsonProperty("refund_amount")
   @JsonSerialize(using = PriceSerializer.class)
   private double refundAmount;
 
