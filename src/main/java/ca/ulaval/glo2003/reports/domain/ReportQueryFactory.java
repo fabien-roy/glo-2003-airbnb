c0c76689e7535e67ad183a1b4f219cd48976f811
Fabien Roy <fabienroy28@gmail.com>
2020-04-19 13:26:09 -0400

    [CRT] Add report query factory tests

--- src/main/java/ca/ulaval/glo2003/reports/domain/ReportQueryFactory.java
+++ src/main/java/ca/ulaval/glo2003/reports/domain/ReportQueryFactory.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import java.util.Set;
 import javax.inject.Inject;
 
-// TODO : Test ReportQueryFactory
 public class ReportQueryFactory {
 
   private final ReportQueryBuilder reportQueryBuilder;
