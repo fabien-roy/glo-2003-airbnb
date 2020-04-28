9d7edc79b69a52640e3e2c47a42d9216e58d2ede
Vincent Masse <vince2848@msn.com>
2020-04-28 16:10:26 -0400

    Add AdminResource

---
+++ src/main/java/ca/ulaval/glo2003/admin/rest/AdminResource.java
@@ -0,0 +1,34 @@
+package ca.ulaval.glo2003.admin.rest;
+
+import static spark.Spark.delete;
+
+import ca.ulaval.glo2003.admin.service.AdminService;
+import javax.inject.Inject;
+import org.eclipse.jetty.http.HttpStatus;
+import spark.Request;
+import spark.Response;
+import spark.RouteGroup;
+
+public class AdminResource implements RouteGroup {
+
+  public static final String ADMIN_PATH = "/admin";
+
+  private final AdminService adminService;
+
+  @Inject
+  public AdminResource(AdminService adminService) {
+    this.adminService = adminService;
+  }
+
+  @Override
+  public void addRoutes() {
+    delete("", this::deleteAll);
+  }
+
+  public Object deleteAll(Request request, Response response) {
+    adminService.delete();
+    response.status(HttpStatus.OK_200);
+
+    return "";
+  }
+}
