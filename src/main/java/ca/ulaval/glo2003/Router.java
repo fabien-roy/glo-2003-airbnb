67de7ea7807c64df70f60db6aa7c7d13c6ff24d2
Vincent Masse <vince2848@msn.com>
2020-04-28 16:10:38 -0400

    Set path

--- src/main/java/ca/ulaval/glo2003/Router.java
+++ src/main/java/ca/ulaval/glo2003/Router.java
@@ -1,11 +1,13 @@
 package ca.ulaval.glo2003;
 
+import static ca.ulaval.glo2003.admin.rest.AdminResource.ADMIN_PATH;
 import static ca.ulaval.glo2003.beds.rest.BedResource.BED_PATH;
 import static ca.ulaval.glo2003.bookings.rest.BookingResource.BOOKING_PATH;
 import static ca.ulaval.glo2003.errors.rest.ErrorMapper.ERROR_PATH;
 import static ca.ulaval.glo2003.transactions.rest.TransactionResource.TRANSACTION_PATH;
 import static spark.Spark.path;
 
+import ca.ulaval.glo2003.admin.rest.AdminResource;
 import ca.ulaval.glo2003.beds.rest.BedResource;
 import ca.ulaval.glo2003.bookings.rest.BookingResource;
 import ca.ulaval.glo2003.errors.rest.ErrorMapper;
@@ -18,17 +20,20 @@ public class Router {
   private final TransactionResource transactionResource;
   private final BedResource bedResource;
   private final BookingResource bookingResource;
+  private final AdminResource adminResource;
 
   @Inject
   public Router(
       ErrorMapper errorMapper,
       TransactionResource transactionResource,
       BedResource bedResource,
-      BookingResource bookingResource) {
+      BookingResource bookingResource,
+      AdminResource adminResource) {
     this.errorMapper = errorMapper;
     this.transactionResource = transactionResource;
     this.bedResource = bedResource;
     this.bookingResource = bookingResource;
+    this.adminResource = adminResource;
   }
 
   public void setUpRoutes() {
@@ -36,5 +41,6 @@ public class Router {
     path(TRANSACTION_PATH, transactionResource);
     path(BED_PATH, bedResource);
     path(BOOKING_PATH, bookingResource);
+    path(ADMIN_PATH, adminResource);
   }
 }
