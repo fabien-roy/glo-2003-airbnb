f4ede31f97d8519b280b222f3e15d77380a82a15
Vincent Masse <vince2848@msn.com>
2020-04-29 14:37:08 -0400

    [CRT] Add transactionService to AdminModule

--- src/main/java/ca/ulaval/glo2003/admin/AdminModule.java
+++ src/main/java/ca/ulaval/glo2003/admin/AdminModule.java
@@ -2,6 +2,7 @@ package ca.ulaval.glo2003.admin;
 
 import ca.ulaval.glo2003.admin.service.AdminService;
 import ca.ulaval.glo2003.beds.services.BedService;
+import ca.ulaval.glo2003.transactions.services.TransactionService;
 import com.google.inject.AbstractModule;
 
 public class AdminModule extends AbstractModule {
@@ -10,5 +11,6 @@ public class AdminModule extends AbstractModule {
   protected void configure() {
     bind(AdminService.class);
     bind(BedService.class);
+    bind(TransactionService.class);
   }
 }
