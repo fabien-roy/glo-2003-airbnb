260b0434d704784c7f98ee1087f22dcdfc044255
Vincent Masse <vince2848@msn.com>
2020-04-29 13:48:28 -0400

    [CRT] Add deleteAll method

--- src/main/java/ca/ulaval/glo2003/admin/service/AdminService.java
+++ src/main/java/ca/ulaval/glo2003/admin/service/AdminService.java
@@ -1,5 +1,17 @@
 package ca.ulaval.glo2003.admin.service;
 
+import ca.ulaval.glo2003.beds.services.BedService;
+import javax.inject.Inject;
+
 public class AdminService {
-  public void deleteAll() {};
+  private BedService bedService;
+
+  @Inject
+  public AdminService(BedService bedService) {
+    this.bedService = bedService;
+  }
+
+  public void deleteAll() {
+    bedService.deleteAll();
+  };
 }
