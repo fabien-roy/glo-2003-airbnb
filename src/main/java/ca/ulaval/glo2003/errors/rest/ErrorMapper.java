9d810638d78f2bbe5b32218aeb5146d10c76ba39
Fabien Roy <fabienroy28@gmail.com>
2020-04-29 19:14:28 -0400

    [CRT] Add transaction exception handling in error mapper

--- src/main/java/ca/ulaval/glo2003/errors/rest/ErrorMapper.java
+++ src/main/java/ca/ulaval/glo2003/errors/rest/ErrorMapper.java
@@ -11,6 +11,8 @@ import ca.ulaval.glo2003.locations.exceptions.LocationException;
 import ca.ulaval.glo2003.locations.rest.handlers.LocationExceptionHandler;
 import ca.ulaval.glo2003.reports.exceptions.ReportException;
 import ca.ulaval.glo2003.reports.rest.handlers.ReportExceptionHandler;
+import ca.ulaval.glo2003.transactions.exceptions.TransactionException;
+import ca.ulaval.glo2003.transactions.rest.handlers.TransactionExceptionHandler;
 import com.google.inject.Inject;
 import spark.RouteGroup;
 
@@ -19,31 +21,35 @@ public class ErrorMapper implements RouteGroup {
   public static final String ERROR_PATH = "*";
 
   private final CatchallExceptionHandler catchallExceptionHandler;
+  private final TransactionExceptionHandler transactionExceptionHandler;
+  private final ReportExceptionHandler reportExceptionHandler;
   private final LocationExceptionHandler locationExceptionHandler;
   private final BedExceptionHandler bedExceptionHandler;
   private final BookingExceptionHandler bookingExceptionHandler;
-  private final ReportExceptionHandler reportExceptionHandler;
 
   @Inject
   public ErrorMapper(
       CatchallExceptionHandler catchallExceptionHandler,
+      TransactionExceptionHandler transactionExceptionHandler,
+      ReportExceptionHandler reportExceptionHandler,
       LocationExceptionHandler locationExceptionHandler,
       BedExceptionHandler bedExceptionHandler,
-      BookingExceptionHandler bookingExceptionHandler,
-      ReportExceptionHandler reportExceptionHandler) {
+      BookingExceptionHandler bookingExceptionHandler) {
     this.catchallExceptionHandler = catchallExceptionHandler;
+    this.transactionExceptionHandler = transactionExceptionHandler;
+    this.reportExceptionHandler = reportExceptionHandler;
     this.locationExceptionHandler = locationExceptionHandler;
     this.bedExceptionHandler = bedExceptionHandler;
     this.bookingExceptionHandler = bookingExceptionHandler;
-    this.reportExceptionHandler = reportExceptionHandler;
   }
 
   @Override
   public void addRoutes() {
     exception(Exception.class, catchallExceptionHandler);
+    exception(TransactionException.class, transactionExceptionHandler);
+    exception(ReportException.class, reportExceptionHandler);
     exception(LocationException.class, locationExceptionHandler);
     exception(BedException.class, bedExceptionHandler);
     exception(BookingException.class, bookingExceptionHandler);
-    exception(ReportException.class, reportExceptionHandler);
   }
 }
