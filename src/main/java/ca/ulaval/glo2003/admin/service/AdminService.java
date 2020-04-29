5b3f4c3b8f98e2c2a891e4ea9d4016dc26495338
Vincent Masse <vince2848@msn.com>
2020-04-29 14:21:27 -0400

    [CRT] Add transactionService.deleteAll to deleteAll method

--- src/main/java/ca/ulaval/glo2003/admin/service/AdminService.java
+++ src/main/java/ca/ulaval/glo2003/admin/service/AdminService.java
@@ -1,17 +1,21 @@
 package ca.ulaval.glo2003.admin.service;
 
 import ca.ulaval.glo2003.beds.services.BedService;
+import ca.ulaval.glo2003.transactions.services.TransactionService;
 import javax.inject.Inject;
 
 public class AdminService {
   private BedService bedService;
+  private TransactionService transactionService;
 
   @Inject
-  public AdminService(BedService bedService) {
+  public AdminService(BedService bedService, TransactionService transactionService) {
     this.bedService = bedService;
+    this.transactionService = transactionService;
   }
 
   public void deleteAll() {
     bedService.deleteAll();
+    transactionService.deleteAll();
   };
 }
