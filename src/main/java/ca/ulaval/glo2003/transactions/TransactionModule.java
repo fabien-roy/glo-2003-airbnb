b3e9ee3179fd4c810e15bbd59b5d1ecff4800436
Hugo Lafleur <gohu.ruelfal88@gmail.com>
2020-04-29 22:00:59 -0400

    FFR_fix_to_exception

--- src/main/java/ca/ulaval/glo2003/transactions/TransactionModule.java
+++ src/main/java/ca/ulaval/glo2003/transactions/TransactionModule.java
@@ -4,6 +4,7 @@ import ca.ulaval.glo2003.transactions.domain.TransactionRepository;
 import ca.ulaval.glo2003.transactions.infrastructure.InMemoryTransactionRepository;
 import ca.ulaval.glo2003.transactions.rest.TransactionMapper;
 import ca.ulaval.glo2003.transactions.rest.TransactionResource;
+import ca.ulaval.glo2003.transactions.rest.serializers.TransactionDeserializingModule;
 import ca.ulaval.glo2003.transactions.rest.serializers.TransactionSerializingModule;
 import ca.ulaval.glo2003.transactions.services.TransactionService;
 import com.google.inject.AbstractModule;
@@ -15,6 +16,7 @@ public class TransactionModule extends AbstractModule {
   protected void configure() {
     bind(TransactionRepository.class).to(InMemoryTransactionRepository.class).in(Singleton.class);
     bind(TransactionSerializingModule.class);
+    bind(TransactionDeserializingModule.class);
     bind(TransactionMapper.class);
     bind(TransactionService.class);
     bind(TransactionResource.class);
--- src/main/java/ca/ulaval/glo2003/transactions/exceptions/OutOfBoundsServiceFeeException.java
+++ src/main/java/ca/ulaval/glo2003/transactions/exceptions/OutOfBoundsServiceFeeException.java
@@ -1,6 +1,6 @@
 package ca.ulaval.glo2003.transactions.exceptions;
 
-public class OutOfBoundsServiceFeeException extends TransactionException {
+public class OutOfBoundsServiceFeeException extends RuntimeException {
 
   public OutOfBoundsServiceFeeException() {
     super("OUT_OF_BOUNDS_SERVICE_FEE");
